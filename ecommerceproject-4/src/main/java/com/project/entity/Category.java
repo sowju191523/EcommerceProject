41c8012079101ba46863618fb04e5f3f5ba1aa0a
sowju191523 <sowju191523@gmail.com>
2025-05-23 17:14:39 +0530

    first commit

---
+++ ecommerceproject-4/src/main/java/com/project/entity/Category.java
@@ -0,0 +1,44 @@
+package com.project.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	public Category() {
+		// TODO Auto-generated constructor stub
+	}
+	public Category(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+	
+
+}
---
+++ ecommerceproject-4/src/main/java/com/project/repository/AdminRepository.java
@@ -0,0 +1,14 @@
+package com.project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.entity.Admin;
+
+public interface AdminRepository  extends JpaRepository<Admin, Integer>{
+ 
+	
+	
+	
+	
+	
+}
